2f529ee63115833dbc944e8cf75e926e0d4c61b7
Ludmila Dombrowska <vanilla77177@gmail.com>
2023-02-20 22:09:50 +0100

    feat: implemented tasks 2 and 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const time = document.querySelector('.time');
-console.log(time);
 
 time.textContent = "Text";
 
@@ -28,8 +27,74 @@ function showDate() {
     document.querySelector('.date').innerHTML = currentDate;
     let tomorrow = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1);
 
-    console.log(tomorrow - date)
     setTimeout(function() { showDate() }, tomorrow - date)
 }
 showTime()
 showDate()
+
+let timeOfDay = ''
+let randnum = 1
+
+function setBg(index) { 
+    const randfile = (index < 10) ? '0' + index : index
+    let url = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${randfile}.jpg`;
+
+    const img = new Image();
+    img.src = url 
+    img.onload = () => {      
+      document.body.style.backgroundImage = `url('${url}')`
+    }; 
+}
+
+function nextBg() {
+    randnum = (randnum == 20) ? 1 : randnum + 1
+    setBg(randnum)
+}
+
+function prevBg() {
+    randnum = (randnum == 0) ? 20 : randnum - 1
+    setBg(randnum)
+}
+
+document.querySelector('.slide-prev').onclick = prevBg;
+document.querySelector('.slide-next').onclick = nextBg;
+
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    
+    switch (Math.floor(hours / 6)) {
+        case 0: timeOfDay = 'night'; break
+        case 1: timeOfDay = 'morning'; break
+        case 2: timeOfDay = 'afternoon'; break
+        case 3: timeOfDay = 'evening'; break
+    }
+    document.querySelector('.greeting').innerHTML = `Good ${timeOfDay}`
+    let nextDate = new Date();
+    nextDate.setMinutes(0)
+    nextDate.setSeconds(0)
+    nextDate.setHours(hours + (6 - (hours % 6)))
+    if (nextDate.getHours() == 24) {
+        nextDate.setHours(0)
+        nextDate += 86400
+    }
+    randnum = (Math.floor(Math.random()*19)+1)
+    
+    setBg(randnum)
+    
+    setTimeout(function() { getTimeOfDay() }, nextDate - date)
+}
+getTimeOfDay()
+
+const name = document.querySelector('.name');
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage)
